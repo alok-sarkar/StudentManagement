4b9f15feeb55636e9d5989434b2e2d3e27490c0c
Alok Sarkar <aloksarkar0002@gmail.com>
2022-06-21 00:57:17 +0530

    [ADD] Custom exception Handling

--- src/main/java/com/test/StudentMangement/StudentManagement/Exceptions/StudentNotFoundException.java
+++ src/main/java/com/test/StudentMangement/StudentManagement/Exceptions/StudentNotFoundException.java
@@ -1,11 +1,9 @@
 package com.test.StudentMangement.StudentManagement.Exceptions;
 
-import java.util.function.Supplier;
-
-public class UserNotFoundException extends Exception {
+public class StudentNotFoundException extends RuntimeException {
     int id;
 
-    public UserNotFoundException(int id) {
+    public StudentNotFoundException(int id) {
         super("Student not found");
         this.id = id;
     }
--- src/main/java/com/test/StudentMangement/StudentManagement/Service/StudentServiceImplementation.java
+++ src/main/java/com/test/StudentMangement/StudentManagement/Service/StudentServiceImplementation.java
@@ -1,6 +1,6 @@
 package com.test.StudentMangement.StudentManagement.Service;
 
-import com.test.StudentMangement.StudentManagement.Exceptions.UserNotFoundException;
+import com.test.StudentMangement.StudentManagement.Exceptions.StudentNotFoundException;
 import com.test.StudentMangement.StudentManagement.Models.Student;
 import com.test.StudentMangement.StudentManagement.Repository.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -21,12 +21,12 @@ public class StudentServiceImplementation implements StudentService{
 
     @Override
     public Student getStudentById(int id) {
-        return studentRepository.findById(id).orElseThrow();
+        return studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id));
     }
 
     @Override
     public Student updateStudent(Student student, int id) {
-        Student existingStudent=studentRepository.findById(id).orElseThrow();
+        Student existingStudent=studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id));
         existingStudent.setEmail(student.getEmail());
         existingStudent.setFirstName(student.getFirstName());
         existingStudent.setLastName(student.getLastName());
@@ -41,7 +41,7 @@ public class StudentServiceImplementation implements StudentService{
 
     @Override
     public void deleteStudent(int id) {
-        studentRepository.findById(id).orElseThrow();
+        studentRepository.findById(id).orElseThrow(()-> new StudentNotFoundException(id));
 
         studentRepository.deleteById(id);
     }
